0dde0c5c2a7ff4bbcd6fcf4c5e75c91e753bc6ae
Alexander Bondarenko <alexander.bondarenko@shippeo.com>
2022-12-26 21:09:36 +0100

    slices output

--- main.py
+++ main.py
@@ -1,1 +1,58 @@
 # Put the code for your API here.
+import pandas as pd
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+
+from starter.features import cat_features
+from starter.ml.data import process_data
+from starter.ml.model import inference, load_model
+
+
+class Item(BaseModel):
+    age: int = Field(example=39)
+    workclass: str = Field(example="State-gov")
+    fnlgt: int = Field(example=77516)
+    education: str = Field(example="Bachelors")
+    education_num: int = Field(example=13)
+    marital_status: str = Field(example="Never-married")
+    occupation: str = Field(example="Adm-clerical")
+    relationship: str = Field(example="Not-in-family")
+    race: str = Field(example="White")
+    sex: str = Field(example="Male")
+    capital_gain: int = Field(example=2174)
+    capital_loss: int = Field(example=0)
+    hours_per_week: int = Field(example=40)
+    native_country: str = Field(example="United-States")
+
+    class Config:
+        arbitrary_types_allowed = True
+
+
+# Instantiate the app.
+app = FastAPI()
+model_data_loaded = load_model("./model", suffix="_rf")
+
+
+# Define a GET on the specified endpoint.
+@app.get("/")
+async def say_hello():
+    return {"greeting": "Hello from the census model!"}
+
+
+@app.post("/infer")
+async def infer(body: Item):
+    df = pd.DataFrame()
+    for col, val in body.dict().items():
+        if "_" in col:
+            col = col.replace("_", "-")
+        df[col] = [val]
+    X, _, _, _ = process_data(
+        df,
+        categorical_features=cat_features,
+        # label="salary",
+        training=False,
+        encoder=model_data_loaded["encoder"],
+        lb=model_data_loaded["lb"],
+    )
+    y_pred = int(inference(model_data_loaded["model"], X=X)[0])
+    return {"pred": y_pred}
---
+++ test_main.py
@@ -0,0 +1,63 @@
+# test_bar.py
+
+import json
+from fastapi.testclient import TestClient
+
+from main import app
+import requests
+
+
+client = TestClient(app)
+
+
+def test_get():
+    r = client.get("/")
+    assert r.status_code == 200
+    assert r.json()["greeting"] == "Hello from the census model!"
+
+
+def test_post():
+    record = dict(
+        age=39,
+        workclass="State-gov",
+        fnlgt=77516,
+        education="Bachelors",
+        education_num=13,
+        marital_status="Never-married",
+        occupation="Adm-clerical",
+        relationship="Not-in-family",
+        race="White",
+        sex="Male",
+        capital_gain=2174,
+        capital_loss=0,
+        hours_per_week=40,
+        native_country="United-States"
+    )
+    data = json.dumps(record)
+    r = client.post("/infer", data=data)
+    assert r.json()["pred"] == 0
+
+
+def test_get_req():
+    record = dict(
+        age=39,
+        workclass="State-gov",
+        fnlgt=77516,
+        education="Bachelors",
+        education_num=13,
+        marital_status="Never-married",
+        occupation="Adm-clerical",
+        relationship="Not-in-family",
+        race="White",
+        sex="Male",
+        capital_gain=2174111,
+        capital_loss=0,
+        hours_per_week=40,
+        native_country="United-States"
+    )
+    data = json.dumps(record)
+    r = requests.post(
+        "http://127.0.0.1:8080/infer",
+        data=data
+    )
+    assert r.json() == {"pred": 1}
