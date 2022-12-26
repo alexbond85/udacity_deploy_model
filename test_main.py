2e66842d8ecab2425fc10e5af14fb8c34246eb17
Alexander Bondarenko <alexander.bondarenko@shippeo.com>
2022-12-26 22:15:41 +0100

    finished

--- test_main.py
+++ test_main.py
@@ -58,3 +58,26 @@ def test_get_req():
     data = json.dumps(record)
     r = requests.post("http://127.0.0.1:8080/infer", data=data)
     assert r.json() == {"pred": 1}
+
+
+def test_get_req_heroku():
+    record = dict(
+        age=39,
+        workclass="State-gov",
+        fnlgt=77516,
+        education="Bachelors",
+        education_num=13,
+        marital_status="Never-married",
+        occupation="Adm-clerical",
+        relationship="Not-in-family",
+        race="White",
+        sex="Male",
+        capital_gain=2174111,
+        capital_loss=0,
+        hours_per_week=40,
+        native_country="United-States",
+    )
+    data = json.dumps(record)
+    r = requests.post("https://alexdemotest.herokuapp.com/infer", data=data)
+    assert r.status_code == 200
+    assert r.json() == {"pred": 1}
